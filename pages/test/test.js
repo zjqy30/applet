009b63a9bd4d6dc6b7a005775c8c7a81225ddfcc
doublestar2017 <xingxing2huo@163.com>
2019-07-10 16:17:55 +0800

    表格demo

--- pages/test/test.js
+++ pages/test/test.js
@@ -2,7 +2,7 @@ import F2 from '../component/f2-canvas/lib/f2';
 
 let chart = null;
 
-function initChart(canvas, width, height) {
+function columnChart(canvas, width, height) {
   const data = [{
       year: '1951 年',
       sales: 38
@@ -63,6 +63,91 @@ function initChart(canvas, width, height) {
   return chart;
 }
 
+function pieChart(canvas, width, height) {
+  const map = {
+    '芳华': '40%',
+    '妖猫传': '20%',
+    '机器之血': '18%',
+    '心理罪': '15%',
+    '寻梦环游记': '5%',
+    '其他': '2%',
+  };
+  const data = [{
+      name: '芳华',
+      percent: 0.4,
+      a: '1'
+    },
+    {
+      name: '妖猫传',
+      percent: 0.2,
+      a: '1'
+    },
+    {
+      name: '机器之血',
+      percent: 0.18,
+      a: '1'
+    },
+    {
+      name: '心理罪',
+      percent: 0.15,
+      a: '1'
+    },
+    {
+      name: '寻梦环游记',
+      percent: 0.05,
+      a: '1'
+    },
+    {
+      name: '其他',
+      percent: 0.02,
+      a: '1'
+    }
+  ];
+  chart = new F2.Chart({
+    el: canvas,
+    width,
+    height
+  });
+  chart.source(data, {
+    percent: {
+      formatter(val) {
+        return val * 100 + '%';
+      }
+    }
+  });
+  chart.legend({
+    position: 'left',
+    itemFormatter(val) {
+      return val + '  ' + map[val];
+    }
+  });
+  chart.tooltip(false);
+  chart.coord('polar', {
+    transposed: true,
+    radius: 0.85
+  });
+  chart.axis(false);
+  chart.interval()
+    .position('a*percent')
+    .color('name', ['#858585', '#13C2C2', '#2FC25B', '#FACC14', '#F04864', '#8543E0'])
+    .adjust('stack')
+    .style({
+      lineWidth: 0.5,
+      stroke: '#fff',
+      lineJoin: 'round',
+      lineCap: 'round'
+    })
+    .animate({
+      appear: {
+        duration: 1000,
+        easing: 'bounceOut'
+      }
+    });
+
+  chart.render();
+  return chart;
+}
+
 Page({
   onShareAppMessage: function(res) {
     return {
@@ -73,8 +158,11 @@ Page({
     }
   },
   data: {
-    opts: {
-      onInit: initChart
+    columnChart: {
+      onInit: columnChart
+    },
+    pieChart: {
+      onInit: pieChart
     }
   },
 
